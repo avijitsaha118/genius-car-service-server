5aa9bb4468a5988598226af2815699679722e6da
avijitsaha118 <sahaavijit908@gmail.com>
2022-04-26 08:25:12 +0600

    updated

--- index.js
+++ index.js
@@ -114,9 +114,11 @@ run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('Running genius server');
 });
+
 app.get('/hero', (req, res) => {
     res.send('server meets heroku');
-})
+});
+
 app.listen(port, () => {
     console.log('listening to port', port);
-})
+});
